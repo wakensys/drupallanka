ea6efaf7384becbf9e8baf81e0c71b8933a438c7
Wakensys <dwijerathna@wakensys.com>
2011-11-08 11:04:06 +0530

    dumi chan

--- index.php
+++ index.php
@@ -19,3 +19,6 @@ define('DRUPAL_ROOT', getcwd());
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 menu_execute_active_handler();
+
+
+//dudmfsif sdfk
